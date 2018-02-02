5db76bc07900775eb96fdeca9df3774506e92661
Antoine Brisebois-Roy <antoine.brisebois-roy@polymtl.ca>
2018-02-01 20:34:17 -0900

    [S1] ABR: Ajout de la course au menu.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CrosswordViewComponent} from './crossword/game-view/crossword-view.component'
 import {DashboardComponent} from './dashboard/dashboard.component'
+import {GameComponent} from './race/game-component/game.component'
 
 const routes: Routes = [
   // { path: '', redirectTo: '/crossword', pathMatch: 'full' },
   {path: '', component:DashboardComponent},
-  {path: 'crossword', component:CrosswordViewComponent}
+  {path: 'crossword', component:CrosswordViewComponent},
+  {path: 'race', component:GameComponent}
 ];
 
 @NgModule({
