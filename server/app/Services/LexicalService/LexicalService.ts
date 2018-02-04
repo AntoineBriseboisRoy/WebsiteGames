f8bed80e1acf2bc5103cfd4604c4912a17970de1
William Harvey <william.harvey@polymtl.ca>
2018-02-04 01:56:41 -0500

    [S1] WH: Code conforme avec TSLint

--- server/app/Services/LexicalService/LexicalService.ts
+++ server/app/Services/LexicalService/LexicalService.ts
@@ -1,6 +1,6 @@
 import * as requestPromise from "request-promise-native";
 import * as fs from "file-system";
-import { WordAndDefinition } from "./WordAndDefinition";
+import { DictionaryEntry } from "./Interfaces";
 
 const EASY: number = 5;
 const MEDIUM: number = 1;
@@ -9,29 +9,32 @@ const HARD: number = 0;
 export class LexicalService {
     private requestResult: JSON;
     private options = {
-        method: 'GET',
+        method: "GET",
         uri: "https://api.datamuse.com/words",
         qs: {
-            sp: "", //-> uri + "?sp=word"
+            sp: "", // -> uri + "?sp=word"
             md: "df"
         },
         json: true,  // automatically parse data to JSON format
         simple: true // should request promise for status code other than 2xx
     };
 
-    private sendGetRequest(searchedTemplate:string) {
+    private sendGetRequest(searchedTemplate: string): Promise<JSON> {
         this.options.qs.sp = searchedTemplate;
+
         return requestPromise(this.options)
-            .then((data: JSON) => {
-                if(Object.keys(data).length === 0)
-                    throw new Error("Il n'existe aucun mot pour cette requete"); 
+            .then( (data: JSON) => {
+                if (Object.keys(data).length === 0) {
+                    throw new Error("Il n'existe aucun mot pour cette requete");
+                }
                 this.requestResult = data;
+
                 return data;
-            },(reject: Error) => {
+            },     (reject: Error) => {
                 throw reject;
             })
             .catch((error: Error) => {
-                console.log(error.message); 
+                console.log(error.message);
         });
     }
 
@@ -39,10 +42,10 @@ export class LexicalService {
         return this.requestResult[index].hasOwnProperty("defs");
     }
 
-    private isInFrequencyInterval(difficulty: number, wordFrequency: number): boolean{
+    private isInFrequencyInterval(difficulty: number, wordFrequency: number): boolean {
         switch (difficulty) {
             case EASY:
-                return wordFrequency >= EASY; 
+                return wordFrequency >= EASY;
             case MEDIUM:
                 return (wordFrequency < EASY) && (wordFrequency >= MEDIUM);
             case HARD:
@@ -53,29 +56,37 @@ export class LexicalService {
     }
 
     private isValidDifficulty(index: number, difficulty: number): boolean{
-        const BEG_FRQ_STR = 2, END_FRQ_STR = 6;
+        const BEG_FRQ_STR: number = 2, END_FRQ_STR: number = 6;
         try{
-            let wordFrequency: number = parseFloat(this.requestResult[index].tags[0].substr(BEG_FRQ_STR, END_FRQ_STR));
-            return (this.isInFrequencyInterval(difficulty, wordFrequency));         
-        }catch(err){throw err};
+            const wordFrequency: number = parseFloat(this.requestResult[index].tags[0].substr(BEG_FRQ_STR, END_FRQ_STR));
+
+            return (this.isInFrequencyInterval(difficulty, wordFrequency));
+        } catch (err) { throw err; }
     }
 
-    private filterWords (difficulty : number): Array<WordAndDefinition> {
-        let result: Array<WordAndDefinition> = new Array<WordAndDefinition>();
-        const LENGTH = Object.keys(this.requestResult).length;
-        try{
-            for( let i:number = 0; i < LENGTH; i++) {
-                if(this.hasDefinition(i) && this.isValidDifficulty(i, difficulty)){
-                    result.push(new WordAndDefinition(this.requestResult[i]));
+    private filterWords (difficulty: number): Array<DictionaryEntry> {
+        const result: Array<DictionaryEntry> = new Array<DictionaryEntry>();
+        const LENGTH: number = Object.keys(this.requestResult).length;
+        try {
+            for ( let i: number = 0; i < LENGTH; i++) {
+                if (this.hasDefinition(i) && this.isValidDifficulty(i, difficulty)){
+                    result.push({word: this.requestResult[i].word, definition: this.requestResult[i].defs});
                 }
             }
-        }catch(err){console.log(err.message); throw err;};
+        } catch (err) { console.log(err.message); throw err; }
 
         return result;
     }
 
-    public async searchWord(searchedTemplate: string, difficulty: number): Promise<Array<WordAndDefinition>> {
-        await this.sendGetRequest(searchedTemplate)
+    public async searchWord(searchedTemplate: string, difficulty: number): Promise<Array<DictionaryEntry>> {
+        await this.sendGetRequest(searchedTemplate);
+        fs.writeFileSync("res.json", JSON.stringify(this.requestResult, null, 2));
+
         return this.filterWords(difficulty);
     }
 }
+
+const service: LexicalService = new LexicalService();
+service.searchWord("a?p??", HARD).then((result: Array<DictionaryEntry>) => {
+    console.log(result[4].definition[0]);
+});
