22464ce1fb09af553dd5e95aef18a41d01804bac
JustinGod <33948903+JustinGod@users.noreply.github.com>
2018-02-07 20:47:30 -0500

    [S2] OF: Ajout d'une verification s'assurant que tous les mots sont relies dans la generation de BlackSquares. Fonction trop longue, a revoir

--- server/app/Grid/Constants.ts
+++ server/app/Grid/Constants.ts
@@ -7,7 +7,7 @@ export const MIN_WORDS_PER_LINE: number = 1;
 export const MAX_WORDS_PER_LINE: number = 3;
 export const MAX_BLACKSQUARE_RATIO: number = 0.6;
 export const NOT_FOUND: string = "NOT_FOUND_ERR";
-export const PERCENTAGE_BLACK_SQUARES: number = 0.3;
+export const PERCENTAGE_BLACK_SQUARES: number = 0.4;
 export const MAX_WORD_QUERY_ATTEMPS: number = 5;
 export const STANDARD_SIDE_SIZE: number = 10;
 
--- server/app/Grid/GridFiller.spec.ts
+++ server/app/Grid/GridFiller.spec.ts
@@ -5,9 +5,9 @@ import { DictionaryEntry, Constraint } from "./Interfaces";
 import * as cst from "./Constants";
 
 describe("Verifying 2x2 Grid (no black squares)", () => {
-    it.only("Should be full (4 letters)", () => {
+    it("Should be full (4 letters)", () => {
         let nLetters: number = 0;
-        const SIDE_SIZE: number = 10, BLACK_SQUARE_RATIO: number = 0.45, SQUARE: number = 2;
+        const SIDE_SIZE: number = 4, BLACK_SQUARE_RATIO: number = 0, SQUARE: number = 2;
         const testGrid: Grid = new Grid(SIDE_SIZE, BLACK_SQUARE_RATIO);
         testGrid.GridContent.forEach((row: string[]) => {
             row.forEach((letter: string) => {
