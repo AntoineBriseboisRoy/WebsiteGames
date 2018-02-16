4ce593df1a1959ded3ea7f6df8e5867532df12c0
Claudia Onorato <claudia.onorato@polymtl.ca>
2018-02-16 10:05:14 -0500

    [S2]CO: Ajustement des dimensions du canvas selon la fenêtre

--- client/src/app/race/edit-track/edit-track.component.ts
+++ client/src/app/race/edit-track/edit-track.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Point } from "./Point";
 import * as cst from "../../constants";
+import { IterableChangeRecord_ } from "@angular/core/src/change_detection/differs/default_iterable_differ";
 
 @Component({
   selector: "app-edit-track",
@@ -18,17 +19,26 @@ export class EditTrackComponent implements OnInit {
 
   public ngOnInit(): void {
     this.points = new Array<Point>();
-    this.canvas = document.getElementById("tutorial") as HTMLCanvasElement;
+    this.canvas = document.getElementById("edit") as HTMLCanvasElement;
     this.context = this.canvas.getContext("2d");
+    this.resizeWindow();
 
+    window.addEventListener("resize", () => this.resizeWindow());
     this.canvas.addEventListener("click", (event: MouseEvent) => this.handleCreateNewPoint(event));
     this.canvas.addEventListener("contextmenu", (event: MouseEvent) => this.handleDeleteLastPoint(event));
-    this.canvas.addEventListener("mousedown", (event: MouseEvent) => {
+   /*  this.canvas.addEventListener("mousedown", (event: MouseEvent) => {
       this.mousePressed = true;
       this.handleStartDrag(event);
     });
-    this.canvas.addEventListener("mouseup", () => this.mousePressed = false);
+    this.canvas.addEventListener("mouseup", () => this.mousePressed = false); */
     this.canvas.oncontextmenu = () => false ;
+  }
+
+  private resizeWindow(): void {
+      this.canvas.width = window.innerWidth - 10;
+      this.canvas.height = window.innerHeight - 10;
+      this.drawRoads();
+      this.drawPoints();
   }
 
   /*private drawimage(): void {
@@ -42,15 +52,12 @@ export class EditTrackComponent implements OnInit {
   }*/
 
   private handleStartDrag(event: MouseEvent): void {
-    
-
     /*while (this.mousePressed) {
       console.log("boucle");
     }*/
   }
 
   private handleCreateNewPoint(event: MouseEvent): void {
-
     if (this.isEndPoint(event.x, event.y)) {
       this.points.push(new Point(this.points[0].x, this.points[0].y, false, true));
     } else if (this.points.length === 0) {
@@ -67,7 +74,7 @@ export class EditTrackComponent implements OnInit {
   }
 
   private redrawCanvas(): void {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.clearRect(0, 0, this.canvas.getBoundingClientRect().width, this.canvas.getBoundingClientRect().width);
     this.drawRoads();
     this.drawPoints();
   }
@@ -75,8 +82,6 @@ export class EditTrackComponent implements OnInit {
   private drawRoads(): void {
     this.context.lineWidth = cst.DEFAULT_LINE_WIDTH;
     this.context.beginPath();
-
-    this.context.moveTo(this.points[0].x, this.points[0].y);
     this.context.strokeStyle = "red";
     for (let i: number = 1; i < this.points.length; ++i) {
       this.context.beginPath();
@@ -94,6 +99,7 @@ export class EditTrackComponent implements OnInit {
       this.context.lineWidth = cst.DEFAULT_LINE_WIDTH;
       this.context.lineJoin = "round";
       this.context.fillStyle = "black";
+      console.log(iterator.x, iterator.y);
       this.context.arc(iterator.x, iterator.y, cst.DEFAULT_CIRCLE_RADIUS, 0, cst.FULL_CIRCLE_RAD, false);
       this.context.strokeStyle = (iterator.start) ? "red" : "blue";
       this.context.stroke();
@@ -103,7 +109,6 @@ export class EditTrackComponent implements OnInit {
   }
 
   private isEndPoint(x: number, y: number): boolean {
-
     return (this.points.length !== 0 &&
             this.points[0].x + cst.TWICE_DEFAULT_CIRCLE_RADIUS > x &&
             this.points[0].x - cst.TWICE_DEFAULT_CIRCLE_RADIUS < x &&
