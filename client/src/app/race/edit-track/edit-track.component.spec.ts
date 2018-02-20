3537ef47897cf6c2c0ffde38b0717441bdfe01b6
Olivier <33948903+JustinGod@users.noreply.github.com>
2018-02-20 10:42:03 -0500

    [S2] CO+OF: Creation d'un test a partir de generation de MouseEvent

--- client/src/app/race/edit-track/edit-track.component.spec.ts
+++ client/src/app/race/edit-track/edit-track.component.spec.ts
@@ -1,16 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { EditTrackComponent } from "./edit-track.component";
 
-import { EditTrackComponent } from './edit-track.component';
+const createPoint: Function = (x: number, y: number) => {
+  const oEvent: MouseEvent = document.createEvent("MouseEvent");
+  oEvent.initMouseEvent("mousedown", true, true, document.defaultView, 1,
+                        x, y, x, y, false, false, false, false, 1, null);
+  const elem: HTMLCanvasElement = document.getElementById("edit") as HTMLCanvasElement;
+  elem.dispatchEvent(oEvent);
+  oEvent.initMouseEvent("mouseup", true, true, document.defaultView, 1,
+                        x, y, x, y, false, false, false, false, 1, null);
+  elem.dispatchEvent(oEvent);
+};
 
-describe('EditTrackComponent', () => {
+describe("EditTrackComponent", () => {
   let component: EditTrackComponent;
   let fixture: ComponentFixture<EditTrackComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditTrackComponent ]
+      declarations: [EditTrackComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +28,8 @@ describe('EditTrackComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should create a point", () => {
+    createPoint(15, 15);
+    expect(component.Points.length).toBe(1);
   });
 });
