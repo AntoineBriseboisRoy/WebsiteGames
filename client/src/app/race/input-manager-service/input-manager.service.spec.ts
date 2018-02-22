ed15a3280ea022e4c24fcf61433f0f424618ab7d
Monssaf Toukal <tmonssaf@gmail.com>
2018-02-21 21:57:35 -0500

    [S2] MT: test sur acceleration/deceleration de la voiture

--- client/src/app/race/input-manager-service/input-manager.service.spec.ts
+++ client/src/app/race/input-manager-service/input-manager.service.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed, inject } from "@angular/core/testing";
 
-import { InputManagerService } from "./input-manager.service";
+import {
+  InputManagerService, ACCELERATE_KEYCODE, LEFT_KEYCODE,
+  BRAKE_KEYCODE, RIGHT_KEYCODE, ZOOM_IN_KEYCODE,
+  ZOOM_OUT_KEYCODE, SWAP_CAM_KEYCODE
+} from "./input-manager.service";
+import {Car} from "../car/car";
+import { CameraContext } from "../camera/camera-context";
+import {Vector3} from "three";
 
-describe("CarControlService", () => {
+const car: Car = new Car();
+const MS_BETWEEN_FRAMES: number = 16.6667;
+const cameraContext: CameraContext = new CameraContext();
+const service: InputManagerService = new InputManagerService();
+const INVALID_KEYCODE: number = 123;
+
+describe("InputManagerService", () => {
+  service.init(car, cameraContext);
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [InputManagerService]
     });
   });
 
-  it("should be created", inject([InputManagerService], (service: InputManagerService) => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it("should accelerate the car", () => {
+    service.handleKeyDown(ACCELERATE_KEYCODE);
+    expect(car.isAcceleratorPressed).toEqual(true);
+  });
+  it("should stop accelerating the car", () => {
+    service.handleKeyUp(ACCELERATE_KEYCODE);
+    expect(car.isAcceleratorPressed).toEqual(false);
+  });
 });
