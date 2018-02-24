8ee5a60ac23e05aa9d7007170393b491b67b686f
Monssaf Toukal <tmonssaf@gmail.com>
2018-02-23 22:48:42 -0500

    [S2] MT: Ajout des tests sur le controle du bolide

--- client/src/app/race/input-manager-service/input-manager.service.spec.ts
+++ client/src/app/race/input-manager-service/input-manager.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed, inject } from "@angular/core/testing";
-
 import {
   InputManagerService, ACCELERATE_KEYCODE, LEFT_KEYCODE,
   BRAKE_KEYCODE, RIGHT_KEYCODE, ZOOM_IN_KEYCODE,
@@ -8,16 +7,20 @@ import {
 import {Car} from "../car/car";
 import { CameraContext } from "../camera/camera-context";
 import {Vector3} from "three";
+import { CameraState } from "../camera/camera-state";
+import { ThirdPersonCamera } from "../camera/camera-perspective";
 
 const car: Car = new Car();
 const MS_BETWEEN_FRAMES: number = 16.6667;
 const cameraContext: CameraContext = new CameraContext();
 const service: InputManagerService = new InputManagerService();
 const INVALID_KEYCODE: number = 123;
+const ARBITRARY_VECTOR: Vector3 = new Vector3(1, 1, 1);
 
 describe("InputManagerService", () => {
   service.init(car, cameraContext);
   beforeEach(() => {
+    cameraContext.initStates(ARBITRARY_VECTOR);
     TestBed.configureTestingModule({
       providers: [InputManagerService]
     });
@@ -34,5 +37,15 @@ describe("InputManagerService", () => {
   it("should stop accelerating the car", () => {
     service.handleKeyUp(ACCELERATE_KEYCODE);
     expect(car.isAcceleratorPressed).toEqual(false);
+  });
+  it("should call the steerLeft method of Car", () => {
+    spyOn(car, "steerLeft");
+    service.handleKeyDown(LEFT_KEYCODE);
+    expect(car.steerLeft).toHaveBeenCalled();
+  });
+  it("should call the steerRight method of Car", () => {
+    spyOn(car, "steerRight");
+    service.handleKeyDown(RIGHT_KEYCODE);
+    expect(car.steerRight).toHaveBeenCalled();
   });
 });
