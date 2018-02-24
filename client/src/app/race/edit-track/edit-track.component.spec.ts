817554788a7f38c3e921af08cbf317512a74decb
Claudia Onorato <claudia.onorato@polymtl.ca>
2018-02-24 18:44:20 -0500

    [S2] CO: Ajout des tests pour l'édition des pistes

--- client/src/app/race/edit-track/edit-track.component.spec.ts
+++ client/src/app/race/edit-track/edit-track.component.spec.ts
@@ -1,36 +1,82 @@
 /* tslint:disable:no-magic-numbers */
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { EditTrackComponent } from "./edit-track.component";
+import * as cst from "../../constants";
+
+let elem: HTMLCanvasElement;
+const oEvent: MouseEvent = document.createEvent("MouseEvent");
 
 const createPoint: Function = (x: number, y: number) => {
-  const oEvent: MouseEvent = document.createEvent("MouseEvent");
-  oEvent.initMouseEvent("mousedown", true, true, document.defaultView, 1,
-                        x, y, x, y, false, false, false, false, 1, null);
-  const elem: HTMLCanvasElement = document.getElementById("edit") as HTMLCanvasElement;
-  elem.dispatchEvent(oEvent);
-  oEvent.initMouseEvent("mouseup", true, true, document.defaultView, 1,
-                        x, y, x, y, false, false, false, false, 1, null);
-  elem.dispatchEvent(oEvent);
+    createMouseEvent("mousedown", x, y, 0);
+    createMouseEvent("mouseup", x, y, 0);
+};
+
+const createMouseEvent: Function = (nameEvent: string, x: number, y: number, button: number) => {
+    oEvent.initMouseEvent(nameEvent, true, true, document.defaultView, 1,
+                          x, y, x, y, false, false, false, false, button, null);
+    elem.dispatchEvent(oEvent);
 };
 
 describe("EditTrackComponent", () => {
-  let component: EditTrackComponent;
-  let fixture: ComponentFixture<EditTrackComponent>;
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [EditTrackComponent]
-    })
-      .compileComponents();
-  }));
+    let component: EditTrackComponent;
+    let fixture: ComponentFixture<EditTrackComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditTrackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [EditTrackComponent]
+        })
+            .compileComponents();
+    }));
 
-  it("should create a point", () => {
-    createPoint(15, 15);
-    expect(component.Points.length).toBe(1);
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditTrackComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        elem = document.getElementById("edit") as HTMLCanvasElement;
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should create a point", () => {
+        createPoint(15, 15);
+        expect(component.Points.length).toBe(1);
+    });
+
+    it("should identify the first point as the start point", () => {
+        createPoint(15, 15);
+        expect(component.Points[0].start).toBeTruthy();
+        expect(component.Points[0].end).toBeFalsy();
+    });
+
+    it("should close the track if a point is placed on the first point", () => {
+        createPoint(15, 15);
+        createPoint(15, 45);
+        createPoint(5, 85);
+        createPoint(15, 15);
+        expect(component.Points.length).toBe(4);
+        expect(component.Points[component.Points.length - 1].start).toBeFalsy();
+        expect(component.Points[component.Points.length - 1].end).toBeTruthy();
+    });
+
+    it("should move point if it's being dragged", () => {
+        createPoint(15, 15);
+        createPoint(15, 45);
+        createPoint(5, 85);
+        createMouseEvent("mousedown", 15, 45, 0);
+        createMouseEvent("mousemove", 140, 25, 0);
+        expect(component.Points[component.Points.length - 2].x).toBe(140);
+        expect(component.Points[component.Points.length - 2].y).toBe(25);
+    });
+
+    it("should delete last point if right click", () => {
+        createPoint(15, 15);
+        createPoint(15, 45);
+        createPoint(5, 85);
+        createPoint(15, 15);
+        createMouseEvent("mousedown", 15, 45, cst.RIGHT_MOUSE_BUTTON);
+        createMouseEvent("mouseup", 15, 45, cst.RIGHT_MOUSE_BUTTON);
+        expect(component.Points.length).toBe(3);
+    });
 });
