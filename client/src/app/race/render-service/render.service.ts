c9d2ab9913da0db3b1577eed4ae15fd48a8915a2
Olivier <33948903+JustinGod@users.noreply.github.com>
2018-03-07 15:57:59 -0500

    [S3] WH+OF: Ajout de la génération de pistes à partir de points (normalisés)

--- client/src/app/race/render-service/render.service.ts
+++ client/src/app/race/render-service/render.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from "@angular/core";
 import Stats = require("stats.js");
 import { WebGLRenderer, Scene, AmbientLight,
          Mesh, PlaneBufferGeometry, MeshBasicMaterial,
-         DoubleSide, Texture, RepeatWrapping, TextureLoader } from "three";
+         DoubleSide, Texture, RepeatWrapping, TextureLoader, Vector2, BackSide, Vector3 } from "three";
 import { Car } from "../car/car";
 import { ThirdPersonCamera } from "../camera/camera-perspective";
 import { TopViewCamera } from "../camera/camera-orthogonal";
-import { INITIAL_CAMERA_POSITION_Y, FRUSTUM_RATIO, PI_OVER_2 } from "../../constants";
+import { INITIAL_CAMERA_POSITION_Y, FRUSTUM_RATIO, PI_OVER_2, PI_OVER_4 } from "../../constants";
 import { Skybox } from "../skybox/skybox";
 import { CameraContext } from "../camera/camera-context";
+import { Point } from "../edit-track/Geometry";
 
 export const FAR_CLIPPING_PLANE: number = 1000;
 export const NEAR_CLIPPING_PLANE: number = 1;
@@ -16,8 +17,8 @@ export const FIELD_OF_VIEW: number = 70;
 
 const WHITE: number = 0xFFFFFF;
 const AMBIENT_LIGHT_OPACITY: number = 0.5;
-const TEXTURE_TILE_SIZE: number = 10;
-const TEXTURE_SIZE: number = 100;
+const TEXTURE_TILE_REPETIONS: number = 100;
+const TEXTURE_SIZE: number = 1000;
 
 @Injectable()
 export class RenderService {
@@ -28,6 +29,7 @@ export class RenderService {
     private scene: THREE.Scene;
     private stats: Stats;
     private lastDate: number;
+    private points: Point[];
 
     public get car(): Car {
         return this._car;
@@ -41,10 +43,11 @@ export class RenderService {
         this._car = new Car();
     }
 
-    public async initialize(container: HTMLDivElement): Promise<void> {
+    public async initialize(container: HTMLDivElement, points: Point[]): Promise<void> {
         if (container) {
             this.container = container;
         }
+        this.points = points;
 
         await this.createScene();
         this.initStats();
@@ -69,6 +72,7 @@ export class RenderService {
         this.lastDate = Date.now();
     }
 
+    // tslint:disable-next-line:max-func-body-length
     private async createScene(): Promise<void> {
         this.scene = new Scene();
         this.cameraContext = new CameraContext();
@@ -93,15 +97,33 @@ export class RenderService {
         const skybox: Skybox = new Skybox();
         this.scene.background = skybox.CubeTexture;
         this.scene.add(this.createFloorMesh());
+        this.generateTrack();
+    }
+
+    private generateTrack(): void {
+        const trackTexture: Texture = new TextureLoader().load("/assets/camero/road.jpg");
+        trackTexture.wrapS = trackTexture.wrapT = RepeatWrapping;
+        trackTexture.repeat.set(1, 1);
+        for (let i: number = 0; i < this.points.length - 1; ++i) {
+            let vector: Vector2 = new Vector2(this.points[i + 1].x - this.points[i].x, this.points[i + 1].y - this.points[i].y);
+            const mesh: Mesh = new Mesh(new PlaneBufferGeometry(vector.length() * TEXTURE_SIZE, 10),
+                                        new MeshBasicMaterial({ map: trackTexture, side: BackSide }));
+            mesh.position.x = -(this.points[i].y + vector.y / 2) * TEXTURE_SIZE;
+            mesh.position.z = -(this.points[i].x + vector.x / 2) * TEXTURE_SIZE;
+            mesh.rotation.x = PI_OVER_2;
+            mesh.rotation.z = vector.y === 0 ? PI_OVER_2 : Math.atan(vector.x / vector.y);
+            this.scene.add(mesh);
+        }
     }
 
     private createFloorMesh(): Mesh {
         const floorTexture: Texture = new TextureLoader().load("/assets/camero/floor-texture.jpg");
         floorTexture.wrapS = floorTexture.wrapT = RepeatWrapping;
-        floorTexture.repeat.set(TEXTURE_TILE_SIZE, TEXTURE_TILE_SIZE);
+        floorTexture.repeat.set(TEXTURE_TILE_REPETIONS, TEXTURE_TILE_REPETIONS);
         const mesh: Mesh = new Mesh(new PlaneBufferGeometry(TEXTURE_SIZE, TEXTURE_SIZE, 1, 1),
-                                    new MeshBasicMaterial({ map: floorTexture, side: DoubleSide }));
+                                    new MeshBasicMaterial({ map: floorTexture, side: BackSide }));
         mesh.rotation.x = PI_OVER_2;
+        mesh.position.y = -0.01;
 
         return mesh;
     }
