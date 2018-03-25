387a81b86c1b7fa5808151afb2a4643f8c6da4ae
Claudia Onorato <claudia.onorato@polymtl.ca>
2018-03-25 17:05:00 -0400

    [S3] CO: Suppression des imports et attributs inutiles

--- client/src/app/crossword/grid.service.ts
+++ client/src/app/crossword/grid.service.ts
@@ -1,24 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-
 import { Observable } from "rxjs/Observable";
-import { catchError } from "rxjs/operators";
-import { of } from "rxjs/observable/of";
-
 import { SocketIoService } from "./socket-io.service";
-import { GameManager } from "./game-manager";
 import { IGridWord } from "../../../../common/interfaces/IGridWord";
 import { ICell } from "../../../../common/interfaces/ICell";
 import { Observer } from "rxjs/Observer";
 
 @Injectable()
 export class GridService {
-    private readonly BASE_URL: string;
     public gridCells: Array<ICell>;
     public gridWords: Array<IGridWord>;
 
-    public constructor(private http: HttpClient, private socketIO: SocketIoService) {
-        this.BASE_URL = "http://localhost:3000/";
+    public constructor(private socketIO: SocketIoService) {
         this.gridCells = new Array();
         this.gridWords = new Array();
     }
