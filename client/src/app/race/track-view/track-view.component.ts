461efe821535dc3afd4a4003cbaa3708be5f1ebd
Olivier <33948903+JustinGod@users.noreply.github.com>
2018-03-26 16:47:00 -0400

    [S3] OF: Avertissemnt lint regle

--- client/src/app/race/track-view/track-view.component.ts
+++ client/src/app/race/track-view/track-view.component.ts
@@ -33,7 +33,7 @@ export class TrackViewComponent implements OnInit {
         );
     }
 
-    private callModal(): void {
+    public callModal(): void {
         this.modalService.open({
             title: "Race preview", message: "Are you sure you want to play this track?\n",
             firstButton: "Play", secondButton: "Cancel", showPreview: true
