b25be423e643fef2fbfdc8611a4cead5eb8a59d9
Monssaf Toukal <tmonssaf@gmail.com>
2018-03-26 11:44:37 -0400

    [S3] MT: Test de RoomManagerService

--- server/app/Services/RoomManagerService/RoomManagerService.ts
+++ server/app/Services/RoomManagerService/RoomManagerService.ts
@@ -65,9 +65,8 @@ export class RoomManagerService {
     }
 
     public addPlayerToRoom(playerName: string, playerId: string, roomName: string): void {
-        const room: Room = this.getRoom(roomName);
-        if (room !== undefined) {
-            const index: number = this.findRoom(room.Name);
+        const index: number = this.findRoom(roomName);
+        if (index !== ID_NOT_FOUND) {
             this.rooms[index].addPlayer(playerName, playerId);
         }
     }
