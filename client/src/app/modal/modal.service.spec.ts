3e94237e3acc4242d0fab39f6630a07fe789bffc
Samuel Arseneault <samarss97@hotmail.com>
2018-03-26 22:41:58 -0400

    [S3] SA: Autre réglage de test 'should create'

--- client/src/app/modal/modal.service.spec.ts
+++ client/src/app/modal/modal.service.spec.ts
@@ -1,15 +1,18 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { ModalService } from './modal.service';
+import { ModalService } from "./modal.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModalStack } from "@ng-bootstrap/ng-bootstrap/modal/modal-stack";
+import { ModalStateService } from "./modal-state.service";
 
-describe('ModalService', () => {
+describe("ModalService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ModalService]
+      providers: [ModalService, NgbModal, NgbModalStack, ModalStateService]
     });
   });
 
-  it('should be created', inject([ModalService], (service: ModalService) => {
+  it("should be created", inject([ModalService], (service: ModalService) => {
     expect(service).toBeTruthy();
   }));
 });
