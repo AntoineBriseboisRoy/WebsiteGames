39f51f20da0a5d7e51c612224ad88c0c19eebce5
Monssaf Toukal <tmonssaf@gmail.com>
2018-04-12 15:26:59 -0400

    [S4] MT-CO: fix de l'affichage de la difficulté dans la gameList

--- client/src/app/crossword/multiplayer-mode/GameList/game-list.component.ts
+++ client/src/app/crossword/multiplayer-mode/GameList/game-list.component.ts
@@ -3,6 +3,8 @@ import { GameRoomManagerService } from "../game-room-manager.service";
 import { SocketIoService } from "../../socket-io.service";
 import { INewGame } from "../../../../../../common/interfaces/INewGame";
 import { Router } from "@angular/router";
+import { Difficulty } from "../../../../../../common/constants";
+import { DifficultyView } from "../../../constants";
 
 @Component({
     selector: "app-game-list",
@@ -12,7 +14,7 @@ export class GameListComponent {
 
     public userJoiner: Array<string>;
     public constructor(public gameRooms: GameRoomManagerService, private socketIO: SocketIoService,
-                       private router: Router) {
+        private router: Router) {
         this.userJoiner = new Array<string>(this.gameRooms.Games.length);
     }
 
@@ -23,12 +25,30 @@ export class GameListComponent {
         return this.isJoinerDefined(index) && !this.gameRooms.isDefined();
     }
     public joinGame(index: number): void {
-        const gameToPlay: INewGame = { userCreator: this.gameRooms.Games[index].userCreator,
-                                       difficulty: this.gameRooms.Games[index].difficulty,
-                                       userJoiner: this.userJoiner[index],
-                                       userCreatorID: this.gameRooms.Games[index].userCreatorID};
+        const gameToPlay: INewGame = {
+            userCreator: this.gameRooms.Games[index].userCreator,
+            difficulty: this.gameRooms.Games[index].difficulty,
+            userJoiner: this.userJoiner[index],
+            userCreatorID: this.gameRooms.Games[index].userCreatorID
+        };
         this.gameRooms.setGame(gameToPlay);
         this.socketIO.PlayMultiplayerGameSubject.next(gameToPlay);
         this.router.navigate(["/crossword/play"]);
+    }
+    public viewDifficulty(difficulty: Difficulty): string {
+        switch (difficulty) {
+            case Difficulty.Easy: {
+                return DifficultyView.Easy;
+            }
+            case Difficulty.Medium: {
+                return DifficultyView.Medium;
+            }
+            case Difficulty.Hard: {
+                return DifficultyView.Hard;
+            }
+            default: break;
+        }
+
+        return "";
     }
 }
