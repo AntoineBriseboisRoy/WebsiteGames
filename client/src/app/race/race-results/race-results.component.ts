3a195ecc01d0704a831f0b198e87a2b082bef93d
Samuel Arseneault <samarss97@hotmail.com>
2018-04-14 23:08:53 -0400

    [S4] ABR-SA: Ajout du bouton save pour les best times

--- client/src/app/race/race-results/race-results.component.ts
+++ client/src/app/race/race-results/race-results.component.ts
@@ -8,6 +8,8 @@ import { DateFormatter } from "../date-formatter";
 
 const N_BEST_TIMES: number = 5;
 const PLAYER_INDEX: number = 0;
+const PLAYER_NAME_PLACEHOLDER: string = "";
+const ANONYMOUS_PLAYER_NAME: string = "[Anonymous Player]";
 
 @Component({
     selector: "app-race-results",
@@ -17,8 +19,10 @@ const PLAYER_INDEX: number = 0;
 export class RaceResultsComponent implements OnInit {
     private carInformations: CarInformation[];
     private trackBestTimes: BestTime[];
+    private bestTimeRanking: number;
+    private hasSavedName: boolean;
 
-    private isInBestTimes: boolean = false;
+    private isInBestTimes: boolean;
     public get CarInformations(): CarInformation[] {
         return this.carInformations;
     }
@@ -27,9 +31,20 @@ export class RaceResultsComponent implements OnInit {
         return this.trackBestTimes;
     }
 
+    public get BestTimeRanking(): number {
+        return this.bestTimeRanking;
+    }
+
+    public get HasSavedName(): boolean {
+        return this.hasSavedName;
+    }
+
     public constructor(private renderService: RenderService, private queryService: MongoQueryService) {
         this.carInformations = new Array<CarInformation>();
         this.trackBestTimes = new Array<BestTime>();
+        this.bestTimeRanking = 0;
+        this.isInBestTimes = false;
+        this.hasSavedName = false;
     }
 
     public ngOnInit(): void {
@@ -49,14 +64,14 @@ export class RaceResultsComponent implements OnInit {
             } else if (b.totalTime < a.totalTime) {
                 return -1;
             } else {
-                return 0;
+                return 1;
             }
         });
     }
 
     private verifyBestTime(): void {
         if (this.carInformations[PLAYER_INDEX].Ranking === 1) {
-            this.trackBestTimes.push({playerName: "Samuel", time: this.carInformations[PLAYER_INDEX].totalTime});
+            this.trackBestTimes.push({playerName: PLAYER_NAME_PLACEHOLDER, time: this.carInformations[PLAYER_INDEX].totalTime});
             this.trackBestTimes.sort((a: BestTime, b: BestTime) => {
                 if (a.time < b.time) {
                     return -1;
@@ -70,10 +85,10 @@ export class RaceResultsComponent implements OnInit {
                 this.trackBestTimes.pop();
             }
             this.inBestTimes();
+            this.findPlayerRank();
         }
         this.renderService.ActiveTrack.bestTimes = this.TrackBestTimes;
         this.renderService.ActiveTrack.nTimesPlayed++;
-        this.editTrackBestTimesDB();
     }
 
     private getTrackBestTimes(): void {
@@ -90,16 +105,26 @@ export class RaceResultsComponent implements OnInit {
         return DateFormatter.DateToMinSecMillisec(time);
     }
 
-    private editTrackBestTimesDB(): void {
+    public editTrackBestTimesDB(): void {
+        if (this.renderService.ActiveTrack.bestTimes[this.bestTimeRanking].playerName === PLAYER_NAME_PLACEHOLDER) {
+            this.renderService.ActiveTrack.bestTimes[this.bestTimeRanking].playerName = ANONYMOUS_PLAYER_NAME;
+        }
         this.queryService.putTrack(this.renderService.ActiveTrack.name, this.renderService.ActiveTrack)
-        .then()
+        .then(() => this.hasSavedName = true)
         .catch((err: Error) => { console.error(err); }
         );
     }
 
     private inBestTimes(): void {
-        this.isInBestTimes = this.carInformations[PLAYER_INDEX].totalTime < this.trackBestTimes[this.trackBestTimes.length - 1].time
-                             || this.trackBestTimes.length < N_BEST_TIMES;
+        this.isInBestTimes = this.carInformations[PLAYER_INDEX].totalTime <= this.trackBestTimes[this.trackBestTimes.length - 1].time;
+    }
+
+    private findPlayerRank(): void {
+        if (this.isInBestTimes) {
+            this.bestTimeRanking = this.trackBestTimes.findIndex((bestTime: BestTime) =>
+            bestTime.playerName === PLAYER_NAME_PLACEHOLDER
+            && bestTime.time === this.carInformations[PLAYER_INDEX].totalTime);
+        }
     }
 
     public replay(): void {
