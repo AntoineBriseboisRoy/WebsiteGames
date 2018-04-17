6f357178420b97eee6d00283093eded0afad52e6
Olivier <33948903+JustinGod@users.noreply.github.com>
2018-04-17 11:20:20 -0400

    [S4] OF: Ajustement des timeout des tests de lexicalService.

--- server/app/Services/LexicalService/LexicalService.spec.ts
+++ server/app/Services/LexicalService/LexicalService.spec.ts
@@ -20,32 +20,34 @@ const DEFINITION_4: string =
 "n\tany of several trees of the genus Populus having leaves on flattened stalks so that they flutter in the lightest wind";
 const DEFINITION_5: string = "n\tusually used in combination: `liverwort'; `milkwort'; `whorlywort'";
 
+const EXTENDED_TIMEOUT: number = 10000;
+
 describe("Verifying LexicalService's searchWords function.", () => {
 
     it("should return an array of WordAndDefinition", async () => {
         return testService.searchWords(WORD_1, Difficulty.Easy).then((result: Array<WordAndDefinition>) => {
             expect(result).to.be.an("Array");
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return a common word with its first definition", async () => {
         return testService.searchWords(WORD_1, Difficulty.Easy).then((result: Array<WordAndDefinition>) => {
             expect(result[0]).to.eql({word: WORD_1, definition: DEFINITION_1});
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return a common word with its random definition (not the first one)", async () => {
         return testService.searchWords(WORD_2, Difficulty.Medium).then((result: Array<WordAndDefinition>) => {
             expect(result[0]).not.eql({word: WORD_2, definition: DEFINITION_2});
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return a common word with its only definition", async () => {
         return testService.searchWords(WORD_3, Difficulty.Medium).then((result: Array<WordAndDefinition>) => {
             expect(result[0]).eql({word: WORD_3, definition:
             DEFINITION_3});
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return nothing if the request returns only non common words.", async () => {
         return testService.searchWords(WORD_4, Difficulty.Medium).then((result: Array<WordAndDefinition>) => {
@@ -53,21 +55,21 @@ describe("Verifying LexicalService's searchWords function.", () => {
             DEFINITION_4});
             expect(result).to.eql(Array());
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return a non-common word with the only definition of the word", async () => {
         return testService.searchWords(SEARCHED_TEMPLATE_1, Difficulty.Hard).then((result: Array<WordAndDefinition>) => {
             expect(result[0]).to.eql({word: WORD_4, definition:
             DEFINITION_4});
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("should return a non-common word with its random definition (not the first one)", async () => {
         return testService.searchWords(SEARCHED_TEMPLATE_2, Difficulty.Hard).then((result: Array<WordAndDefinition>) => {
             expect(result[0]).not.eql({word: WORD_2, definition: DEFINITION_1});
             expect(result[0]).to.eql({word: WORD_5, definition: DEFINITION_5});
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("a call to a Rest API should always return same values", async () => {
         let firstResult: Array<WordAndDefinition>;
@@ -79,7 +81,7 @@ describe("Verifying LexicalService's searchWords function.", () => {
             secondResult = await testService.searchWords(SEARCHED_TEMPLATE_2, Difficulty.Hard);
             expect(firstResult).to.eql(secondResult);
         }
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("verify that invalid requests return an empty array", async () => {
         const INVALID_REQUEST: string = "/////";
@@ -87,13 +89,13 @@ describe("Verifying LexicalService's searchWords function.", () => {
         return testService.searchWords(INVALID_REQUEST, Difficulty.Hard).then((result: Array<WordAndDefinition>) => {
             expect(result).to.be.eql(Array());
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
     it("verify an invalid difficulty should return an empty array", async () => {
 
         return testService.searchWords(SEARCHED_TEMPLATE_3, DIFFICULTY_ERR).then((result: Array<WordAndDefinition>) => {
             expect(result).to.be.eql(Array());
         });
-    });
+    }).timeout(EXTENDED_TIMEOUT);
 
 });
