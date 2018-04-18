55f9c5db635e4d0d5a8af46f38047b8316833b5a
Monssaf Toukal <tmonssaf@gmail.com>
2018-04-18 12:35:42 -0400

    [S4] MT: ajout de test dans end game service

--- client/src/app/crossword/end-game.service.spec.ts
+++ client/src/app/crossword/end-game.service.spec.ts
@@ -1,6 +1,8 @@
-import { TestBed, inject } from "@angular/core/testing";
+import { TestBed, inject, async } from "@angular/core/testing";
 
 import { EndGameService } from "./end-game.service";
+import { GridService } from "./grid.service";
+import { ModalService } from "../modal/modal.service";
 
 describe("EndGameService", () => {
     beforeEach(() => {
@@ -11,5 +13,22 @@ describe("EndGameService", () => {
 
     it("should be created", inject([EndGameService], (service: EndGameService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it("should open a modal when grid is completed", async(inject([GridService, ModalService],
+                                                                  (gridService: GridService, modalService: ModalService) => {
+        gridService.fetchGrid().subscribe(() => {
+            for (const cell of gridService.gridCells) {
+                cell.content = cell.content;
+            }
+            for (const word of gridService.gridWords) {
+                word.isFound = true;
+            }
+            expect(modalService.IsOpen).toEqual(true);
+        });
+    })));
+
+    it("should call all the subscribe dans initSubscriptions", inject([EndGameService], (endGameService: EndGameService) => {
+        const spy: jasmine.Spy = spyOn(endGameService, "initSubscriptions");
     }));
 });
